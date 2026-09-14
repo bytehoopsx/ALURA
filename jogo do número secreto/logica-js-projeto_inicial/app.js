edc52fec44885e6e0c0f917e6d56e1c0d588ea19
bytehoopsx <miguelscopimm@gmail.com>
2026-09-14 11:25:14 -0300

    Add files via upload

--- jogo do número secreto/logica-js-projeto_inicial/app.js
+++ jogo do número secreto/logica-js-projeto_inicial/app.js
@@ -1,14 +1,23 @@
 alert('Boas Vindas ao jogo do Número Secreto');
-let numeroSecreto = 9;
+let numeroSecreto = 5;
 console.log(numeroSecreto)
- let chute = prompt('Escolha um Número entre 1 e 10');
+let chute
 console.log('Valor do chute:', chute);
 
-console.log('Resultado da comparação:', chute == numeroSecreto);
-//se chute for igual ao número secreto
-if (numeroSecreto == chute) {
+//enquanto chute não igual ao num secreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um Número entre 1 e 10');
+    console.log('Resultado da comparação:', chute == numeroSecreto);
+    //se chute for igual ao número secreto
+if (chute == numeroSecreto) {
     alert(`Isso ai! Voce descobriu o número secreto ${numeroSecreto}`);
 } else {
     console.log('Valor do Número Secreto', numeroSecreto)
-    alert('Voce errou, o Número Secreto era: ' + numeroSecreto);
+    if (chute >numeroSecreto ) {
+        alert(`O Número secreto é menor que ${chute}`);
+    } else {
+        alert(`O Número secreto é maior que ${chute}`);
+    }
 }
+}
+
