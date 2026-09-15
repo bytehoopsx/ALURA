5bd4aa28aa9cda9d63c5e9e2b6eb658e521aedd7
bytehoopsx <miguelscopimm@gmail.com>
2026-09-15 10:38:14 -0300

    Add files via upload

--- jogo do número secreto/logica-js-projeto_inicial/app.js
+++ jogo do número secreto/logica-js-projeto_inicial/app.js
@@ -1,23 +1,32 @@
-alert('Boas Vindas ao jogo do Número Secreto');
-let numeroSecreto = 5;
-console.log(numeroSecreto)
-let chute
-console.log('Valor do chute:', chute);
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-//enquanto chute não igual ao num secreto
+// enquanto chute não igual ao numSC
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um Número entre 1 e 10');
-    console.log('Resultado da comparação:', chute == numeroSecreto);
-    //se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Voce descobriu o número secreto ${numeroSecreto}`);
-} else {
-    console.log('Valor do Número Secreto', numeroSecreto)
-    if (chute >numeroSecreto ) {
-        alert(`O Número secreto é menor que ${chute}`);
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // se chute igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
     } else {
-        alert(`O Número secreto é maior que ${chute}`);
+        if (chute > numeroSecreto) {
+            alert(`O Número secreto é Menor que ${chute}`);
+        } else {
+            alert(`O Número secreto é Maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1;
+        tentativas++;
     }
 }
-}
 
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+// if (tentativas > 1) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// } else {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// }
